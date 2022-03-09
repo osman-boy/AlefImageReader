6b24c9bd3bec522242ad024b7eeff1b39773a23b
osman-boy <osulejmanov3@gmail.com>
2022-03-09 21:29:49 +0600

    Initial commit

---
+++ app/src/main/java/kz/osman/alefimagereader/MainActivity.kt
@@ -0,0 +1,15 @@
+package kz.osman.alefimagereader
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * Created by Osman Zhuzhoev at 03.09.2022
+ */
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
